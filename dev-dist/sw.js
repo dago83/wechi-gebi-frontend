b102216a1d71e01d8d41f27cc0647c1c4f27c165
dago83 <85219759+dago83@users.noreply.github.com>
2025-11-20 17:49:12 +0300

    commit1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-417a7fbf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.do275aavhag"
+    "revision": "0.8tc2fmj6be"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
